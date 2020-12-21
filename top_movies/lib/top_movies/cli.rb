909b36f5391b4df262e6e3b4289afc7f7d5720b5
Martin Moy <martinmoy1991@gmail.com>
2020-12-21 08:18:31 +0000

    cli start

--- top_movies/lib/top_movies/cli.rb
+++ top_movies/lib/top_movies/cli.rb
@@ -1,6 +1,25 @@
 class TopMovies::CLI 
+
   def call
-    puts "Tigers"
-    binding.pry
+    display_introduction
+    display_instruction
+    display_ranking 
   end
+  
+  def display_introduction
+    puts "Welcome to the TOP 100 Movies"
+    puts "Which select the genre you want to view?"
+  end
+  
+  def display_instruction
+    puts "Please input the number for the genre you want to view."
+  end
+  
+  
+  
+  def display_ranking
+    puts "Rank: 1 Title: Godfather"
+    puts "Rank: 2 Title: Chicken"
+  end
+  
 end
