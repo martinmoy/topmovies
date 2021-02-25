f46b052a1801a6f1f8bfdee78bbeaf805a29452d
Martin Moy <martinmoy1991@gmail.com>
2021-02-25 22:18:45 +0000

    genres class

--- top_movies/lib/top_movies/genre.rb
+++ top_movies/lib/top_movies/genre.rb
@@ -1,6 +1,14 @@
 class TopMovies::Genres
   attr_accessor :name, :url 
   
+  @@genres = []
+  
+  def initialize (name=nil, url=nil)
+    @name = name
+    @url = url
+    @@genres << self
+  end 
+  
   
   
   
