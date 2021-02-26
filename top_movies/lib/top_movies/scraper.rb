49c1ce0ed9a9f7d9640d76c3a2be1c9de958402a
Martin Moy <martinmoy1991@gmail.com>
2021-02-26 06:54:41 +0000

    scraper genres

--- top_movies/lib/top_movies/scraper.rb
+++ top_movies/lib/top_movies/scraper.rb
@@ -1,12 +1,20 @@
 require_relative  "../top_movies"
+require 'nokogiri'
+require 'open-uri'
 
 class TopMovies::Scraper
   
-  def get_page
+  def self.get_page
     Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/"))
+  end
+  
+  def self.scrape_genre
+    genre = self.get_page.css("ul.dropdown-menu li a").text
+    puts genre
+    
   end
   
   
 end
 
-puts "cats"
+TopMovies::Scraper.scrape_genre
