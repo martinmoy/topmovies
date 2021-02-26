27930657970dbd7c72fbd69a3d20def56f13b036
Martin Moy <martinmoy1991@gmail.com>
2021-02-26 06:45:03 +0000

    scraper updated

--- top_movies/lib/top_movies.rb
+++ top_movies/lib/top_movies.rb
@@ -3,8 +3,8 @@ require_relative "./top_movies/cli.rb"
 require_relative "./top_movies/movies.rb"
 require_relative "./top_movies/scraper.rb"
 require_relative "./top_movies/genre.rb"
-require "pry"
-require "nokogiri"
+require 'pry'
+require 'nokogiri'
 require 'open-uri'
 
 
--- top_movies/lib/top_movies/scraper.rb
+++ top_movies/lib/top_movies/scraper.rb
@@ -1,5 +1,12 @@
-require "nokogiri"
-require "open-uri"
+require_relative  "../top_movies"
 
 class TopMovies::Scraper
+  
+  def get_page
+    Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/"))
+  end
+  
+  
 end
+
+puts "cats"
