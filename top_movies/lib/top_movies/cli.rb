0617a8919b9b732aea81efdb53f73c0799bf7472
Martin Moy <martinmoy1991@gmail.com>
2021-03-03 09:07:43 +0000

    restart 2.0

--- top_movies/lib/top_movies/cli.rb
+++ top_movies/lib/top_movies/cli.rb
@@ -16,15 +16,22 @@ class TopMovies::Cli
   end
   
   def get_genre
-    @genre = ["A", "B", "C", 'D']
+    @genres = ["A", "B", "C", 'D']
   end
   
   def list_genre
     @genres.each.with_index(1) do |genres, index|
-    puts "#{index}." "#{genres.name}"
+      puts "#{index}." "#{genres}"
+    end
   end
   
   def select_genre
+      chosen_genre = gets.strip.to_i
+      if chosen_genre.to_i > 0 && chosen_genre.to_i <= @genres.length
+        puts "Here is the list of the Top 100 Movie for #{@genres[chosen_genre - 1]}"
+      end
   end
+  
+  
 
 end
