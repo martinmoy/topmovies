59c509017b58334462e6ef93c2db1fb8eafea8b4
Martin Moy <martinmoy1991@gmail.com>
2021-03-11 06:17:33 +0000

    finalizing 3000

--- top_movies/lib/top_movies/cli.rb
+++ top_movies/lib/top_movies/cli.rb
@@ -55,7 +55,7 @@ class TopMovies::Cli
         puts "Url: https://www.rottentomatoes.com#{movie.url}"
         puts " "
         puts " "
-        binding.pry
+    
       end
     end
     
@@ -74,7 +74,7 @@ class TopMovies::Cli
   
   def get_info
     @info = ["After the death of his father, T'Challa returns home to the African nation of Wakanda to take his rightful place as king. When a powerful enemy suddenly reappears, T'Challa's mettle as king -- and as Black Panther -- gets tested when he's drawn into a conflict that puts the fate of Wakanda and the entire world at risk. Faced with treachery and danger, the young king must rally his allies and release the full power of Black Panther to defeat his foes and secure the safety of his people.",
-    "Zac Efron","mammomm" ]
+    "Amber is silly", "Amber is crazy" ]
   end
   
   
